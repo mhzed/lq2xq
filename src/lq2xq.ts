84a58b7f75c63400ff502759001a4070c412d341
mhzed <minhongz@gmail.com>
2017-11-13 17:23:41 +0800

    change to be async

--- src/lq2xq.ts
+++ src/lq2xq.ts
@@ -25,13 +25,13 @@ export interface RenderOptions {
   /**
    * for expanding a term or phrase to multiple terms or phrases, default is null.
    */
-  termPhraseExpander? : (termOrPhrase:string, filedName:string|null, isPhrase:boolean) => Array<string>;
+  termPhraseExpander? : (termOrPhrase:string, filedName:string|null, isPhrase:boolean) => Promise<string[]>;
 }
 
 const DefaultRenderOption : RenderOptions = {
   defaultField: '.',
   termPhraseModifier: null
-}
+};
 
 /**
  * The following are private types
@@ -39,13 +39,15 @@ const DefaultRenderOption : RenderOptions = {
 interface Field {
   field : string;
   prefix: string | null;
-};
+}
+
 interface BaseTerm {
   field: Field | null;
   term: string;
   boost: string | null;
   prefix: string | null;
-};
+}
+
 interface Term extends BaseTerm {
   similarity: number | null;
 }
@@ -82,24 +84,24 @@ const renderField = (term: BaseTerm, ctx: RenderContext) : string => {
       fieldname = ctx.option.defaultField;
     return `${fieldname} contains text `;
   } else return "";
-}
+};
 
 const convertWildCard = (term: string) : string => {
   if (/[*?]/.test(term)) {
     return term.replace(/([*])/g, ".*").replace(/[?]/g, ".");
   }
   else return null;
-}
+};
 
-const renderTerm = (term: Term, ctx : RenderContext) : string => {
+const renderTerm = async (term: Term, ctx : RenderContext) : Promise<string> => {
     if (ctx.option.termPhraseExpander) {
-      let expr = _(ctx.option.termPhraseExpander(term.term, term.field?term.field.field:null, false))
+      let expr = _(await ctx.option.termPhraseExpander(term.term, term.field?term.field.field:null, false))
           .map((newTerm:string)=>renderTermClause(_.assign({},term, {term:newTerm}), ctx))
           .join(' ftor ');
       return `(${renderField(term, ctx)}${expr})`;
     } else
       return `(${renderField(term, ctx)}${renderTermClause(term, ctx)})`;
-}
+};
 const renderTermClause = (term: Term, ctx : RenderContext) : string => {
   let result : string;
   let wildCardTerm = convertWildCard(term.term);
@@ -110,30 +112,30 @@ const renderTermClause = (term: Term, ctx : RenderContext) : string => {
   if (term.boost != null) result += ` weight {${term.boost}}`;
   if (term.similarity != null) {
     if (ctx.option.similarityConverter == null)
-      throw new Error("Similarity is not supported")
+      throw new Error("Similarity is not supported");
     else
       result += ` ${ctx.option.similarityConverter(term.similarity)}`;
   }
   if (ctx.option.termPhraseModifier) result += ` ${ctx.option.termPhraseModifier}`;
   return result;
-}
-const renderPhrase = (phrase: Phrase, ctx: RenderContext) : string => {
+};
+const renderPhrase = async (phrase: Phrase, ctx: RenderContext) : Promise<string> => {
   if (ctx.option.termPhraseExpander) {
-    let expr = _(ctx.option.termPhraseExpander(phrase.term, phrase.field?phrase.field.field:null, false))
+    let expr = _(await ctx.option.termPhraseExpander(phrase.term, phrase.field?phrase.field.field:null, false))
         .map((newPhrase:string)=>renderPhraseClause(_.assign({},phrase, {term:newPhrase}), ctx))
         .join(' ftor ');
     return `(${renderField(phrase, ctx)}${expr})`;
   } else
     return `(${renderField(phrase, ctx)}${renderPhraseClause(phrase, ctx)})`
 
-}
+};
 const renderPhraseClause = (phrase: Phrase, ctx: RenderContext) : string => {
   let result = `"${phrase.term}"`;
   if (phrase.boost != null) result += ` weight {${phrase.boost}}`;
   if (phrase.proximity != null) result += ` distance at most ${phrase.proximity} words`;
   if (ctx.option.termPhraseModifier) result += ` ${ctx.option.termPhraseModifier}`;
   return result;
-}
+};
 
 const renderOperator = (op: string, ctx: RenderContext) : string => {
   switch (op) {
@@ -141,27 +143,28 @@ const renderOperator = (op: string, ctx: RenderContext) : string => {
     case 'OR': return ctx.hasField ? 'OR' : 'ftor';
     default : throw new Error(`Operator ${op} is not recognized.`);
   }
-}
+};
+
 const renderRange = (range: Range, ctx: RenderContext) : string => {
   if (range.field == null) throw new Error("Must specify field on range query");
   let field =  range.field.field ;
   let ops = range.inclusive ? ['>=', '<='] : ['>','<'];
   return `(${field}${ops[0]}"${range.term_min}" AND ${field}${ops[1]}"${range.term_max}")`;
-}
+};
 
-const renderExpr = (expr: Expression, ctx: RenderContext) : string => {
+const renderExpr = async (expr: Expression, ctx: RenderContext) : Promise<string> => {
   if (expr.operator == 'NOT') {
     throw new Error("Lucene query's NOT operator is broken, see https://stackoverflow.com/questions/17969461/" +
         "not-operator-doesnt-work-in-query-lucene, use - modifier instead");
   }
-  return `(${render(expr.left, ctx)} ${renderOperator(expr.operator, ctx)} ${render(expr.right, ctx)})`;
-}
+  return `(${await render(expr.left, ctx)} ${renderOperator(expr.operator, ctx)} ${await render(expr.right, ctx)})`;
+};
 
 const isSameField = (t1: BaseTerm, t2: BaseTerm) : boolean => {
   if (t1.field === t2.field) return true;   // both null
   else if (t1.field && t2.field) return t1.field.field == t2.field.field;
   else return false;
-} 
+}; 
 const isTerm = (node: any) : boolean => _.has(node, "similarity");
 const isPhrase = (node: any) : boolean => _.has(node, "proximity");
 const isTermOrPhrase = (node: any) : boolean => isTerm(node) || isPhrase(node);
@@ -169,21 +172,21 @@ const isRange = (node: any) : boolean => _.has(node, 'inclusive');
 const hasOperator = (node: any) : boolean => (true && node.operator);
 const hasLeft = (node: any) : boolean => true && node.left;
 
-const render = (tree:any, ctx: RenderContext) : string => {
+const render = async (tree:any, ctx: RenderContext): Promise<string> => {
   if (tree === undefined) return '';
-  else if (isTerm(tree)) return renderTerm(tree, ctx);
-  else if (isPhrase(tree)) return renderPhrase(tree, ctx);
+  else if (isTerm(tree)) return await renderTerm(tree, ctx);
+  else if (isPhrase(tree)) return await renderPhrase(tree, ctx);
   else if (isRange(tree)) return renderRange(tree, ctx);
-  else if (hasOperator(tree)) return renderExpr(tree, ctx); // binary or unary expression
-  else if (hasLeft(tree)) return render(tree.left, ctx);    // an operator-less (single term) expression
+  else if (hasOperator(tree)) return await renderExpr(tree, ctx); // binary or unary expression
+  else if (hasLeft(tree)) return await render(tree.left, ctx);    // an operator-less (single term) expression
   else throw new Error(`${JSON.stringify(tree, null, '  ')} can't be rendered`);
-}
+};
 
 // scan tree recursively to see if any field exists
 const hasField = (tree:any) : boolean => {
   if (tree === undefined) return false;
   else return tree.field != null || hasField(tree.left) || hasField(tree.right);
-}
+};
 const pressField = (tree:any) => {
   if (tree) {
     if (tree.field) {
@@ -193,15 +196,15 @@ const pressField = (tree:any) => {
     if (tree.left) pressField(tree.left);
     if (tree.right) pressField(tree.right);
   }
-}
+};
 const hasAnd = (tree: any) : boolean => {
   if (tree === undefined) return false;
   else return tree.operator === 'AND' || hasAnd(tree.left) || hasAnd(tree.right);
-}
+};
 const hasPrefix = (tree: any) : boolean => {
   if (tree === undefined) return false;
   else return (tree.prefix) || hasPrefix(tree.left) || hasPrefix(tree.right);
-}
+};
 const reconstructClauses = (tree: any, musts: Array<any>, mustnots: Array<any>) : void => {
   if (tree) {
     if (tree.prefix) {
@@ -213,7 +216,7 @@ const reconstructClauses = (tree: any, musts: Array<any>, mustnots: Array<any>) 
     reconstructClauses(tree.left, musts, mustnots);
     reconstructClauses(tree.right, musts, mustnots);
   }
-}
+};
 
 /**
  * 
@@ -223,7 +226,7 @@ const reconstructClauses = (tree: any, musts: Array<any>, mustnots: Array<any>) 
  * @returns {string}
  *  
  */
-export const lq2xq = (lq: string, option? : RenderOptions) : string => {
+export const lq2xq = async (lq: string, option? : RenderOptions) : Promise<string> => {
   let tree = luceneQueryParser.parse(lq);
   if (!option) option = DefaultRenderOption;
   else option = _.assign({}, DefaultRenderOption, option);  // ensure default values are filled
@@ -238,17 +241,19 @@ export const lq2xq = (lq: string, option? : RenderOptions) : string => {
     let pluses = [], minuses = [];
     reconstructClauses(tree, pluses, minuses);
     if (!hasFieldInExr) {
-      let plusExprs = _.map(pluses, (p) => render(p, new RenderContext(option, hasFieldInExr)));
-      let minusExprs = _.map(minuses, (p) => 'ftnot ' + render(p, new RenderContext(option, hasFieldInExr)));
+      let plusExprs = await Promise.all(_.map(pluses, (p) => render(p, new RenderContext(option, hasFieldInExr))));
+      let minusExprs = await Promise.all(_.map(minuses, (p) => render(p, new RenderContext(option, hasFieldInExr))));
+      minusExprs = _.map(minusExprs, (e) => 'ftnot ' + e);
       result += _.join(_.concat(plusExprs, minusExprs), ' ftand ');
     } else {
       //console.log(JSON.stringify(tree, null, '  '));
-      let plusExprs = _.map(pluses, (p) => render(p, new RenderContext(option, hasFieldInExr)));
-      let minusExprs = _.map(minuses, (p) => 'not' + render(p, new RenderContext(option, hasFieldInExr)));
+      let plusExprs = await Promise.all(_.map(pluses, (p) => render(p, new RenderContext(option, hasFieldInExr))));
+      let minusExprs = await Promise.all(_.map(minuses, (p) => render(p, new RenderContext(option, hasFieldInExr))));
+      minusExprs = _.map(minusExprs, (e) => 'not' + e);
       result += _.join(_.concat(plusExprs, minusExprs), ' AND ');
     }
   } else {
-    result += render(tree, new RenderContext(option, hasFieldInExr));
+    result += await render(tree, new RenderContext(option, hasFieldInExr));
   }
   return result;
-}
+};
